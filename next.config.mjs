785ca6c91702128d373ca4653f4e43e653c72a7e
Trupesh WOS <trupeshs@whiteorangesoftware.com>
2024-03-19 10:36:54 +0530

    some change for build

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: "dist",
+  output: "export",
   images: {
     domains: ["images.wsj.net", "picsum.photos"],
   },
+  swcMinify: true,
 };
 
 export default nextConfig;
